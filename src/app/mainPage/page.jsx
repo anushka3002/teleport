7b9f2724d62e2245e6ef1ac1f6faef2845f63ed3
Anushka <anushka2457@gmail.com>
2024-11-29 02:49:37 +0530

    updated routing

--- src/app/mainPage/page.jsx
+++ src/app/mainPage/page.jsx
@@ -1,11 +1,20 @@
 "use client";
 
+import { useEffect } from "react";
 import Profile from "../profile/page";
 import Step1 from "../register/step1/page";
+import { useRouter } from "next/navigation";
 
 
 const MainPage = () => {
   const authenticated = localStorage.getItem("authenticated");
+  const router = useRouter()
+
+  useEffect(()=>{
+    if(authenticated){
+        router.push('/profile')
+    }
+  },[authenticated])
 
   return (
     <div>
